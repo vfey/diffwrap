cb0d2839b950d72055ced9abbc46c19317f45b24
Vidal Fey <vidal.fey@geneviatechnologies.com>
2018-09-19 17:57:34 +0300

    bug fix related to latest bug fix which wasn't a fix...
    ...at least not complete: did hard-code 'id' argument to 'ID' instead of
    'id.col' as it should be
    cranked up version to 0.1-24

--- R/diffExpr.R
+++ R/diffExpr.R
@@ -624,7 +624,7 @@ diff_expr_extract_contrasts <-
 			pdf(file.path(out.dir, paste(analysis.name, contr, "_plots.pdf", sep="_")), width=11, height=8.5)
 			par(mar = c(6,6,5,3))
 			cat(" MA-plot...\n")
-			out.l$MAplots[[contr]] <- diff_expr_ma_plot(d3, contr, id="ID", sym.col, p.thr, fdr.thr, logfc.thr, numlab, out.dir, analysis.name, point.lab, biom.attributes, font.size, lists)
+			out.l$MAplots[[contr]] <- diff_expr_ma_plot(d3, contr, id.col, sym.col, p.thr, fdr.thr, logfc.thr, numlab, out.dir, analysis.name, point.lab, biom.attributes, font.size, lists)
 
 			## Volcano plot
 			cat(" Volcano plot...\n")

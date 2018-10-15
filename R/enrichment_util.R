311c91ecbf39f5f270fd8d78ffd568973092370a
MeeriP <meeri.pekkarinen.genevia@gmail.com>
2018-10-15 10:12:20 +0300

    Added clusterProfilerGO -parameters as a list, re-structured the function calls.
    adding and removing some print-commands for testing

--- R/enrichment_util.R
+++ R/enrichment_util.R
@@ -7,11 +7,20 @@ runEnrichmentAnalyses <- function(diffr_wrapper.output, analysis.name="",
                                   species="human",
                                   p.thr=0.05, fdr.thr=0.05, logfc.thr=1, 
                                   enrichment.methods = c("clusterProfilerGO", "DAVID", "gProfileR", "topGO"), 
-                                  clusterProfiler.params = list(do.similarity.filtering=F,max.gene.set.size = 1000),
+                                  clusterProfilerGO.params = list(do.similarity.filtering=F,
+                                                                  min.gene.set.size=10,
+                                                                  max.gene.set.size = 1000, 
+                                                                  ontology="BP", 
+                                                                  min.overlap=2,
+                                                                  p.adjust.method="BH"),
+                                  clusterProfilerKEGG.params = list(),
                                   david.params = list(email.address="", 
-                                                      url="", 
+                                                      url="",
+                                                      time.out.value = 60000,
+                                                      annotation.category = "GOTERM_BP_FAT",
                                                       max.gene.set.size = 1000),
                                   gProfiler.params = list(),
+                                  topGO.params = list(),
                                   ontologies.used = c("BP")) 
 {
   
@@ -71,16 +80,23 @@ runEnrichmentAnalyses <- function(diffr_wrapper.output, analysis.name="",
        org.db = as.character(enrich.resource.terms[species, method])
        fname.no.suffix = file.path(out.dir, paste(analysis.name, contrast, method, "ORA", sep = "_"))
 
+       # print(clusterProfilerGO.params$ontology)
+       # print(typeof(clusterProfilerGO.params$ontology))
+       # print(clusterProfilerGO.params$p.adjust.method)
+       # print(typeof(clusterProfilerGO.params$p.adjust.method))
+       
        enrichment_out.l$clusterProfiler_ORA[[contrast]] = run_clusterProfiler_GO(input_genes = input.genes,
                                                                                  background_genes = background.genes,
                                                                                  file_name = fname.no.suffix,
-                                                                                 ordered_query = FALSE,
-                                                                                 ontology = ontologies.used,
+                                                                                 ordered_query = FALSE, #to run function in ORA-mode
                                                                                  OrgDb = org.db,
                                                                                  pvalueCutoff = p.thr,
-                                                                                 max_set_size = clusterProfiler.params$max.gene.set.size,
-                                                                                 pAdjustMethod = "BH",
-                                                                                 similarity_filtering = clusterProfiler.params$do.similarity.filtering)
+                                                                                 ontology = clusterProfilerGO.params$ontology,
+                                                                                 min_set_size = clusterProfilerGO.params$min.gene.set.size,
+                                                                                 max_set_size = clusterProfilerGO.params$max.gene.set.size,
+                                                                                 min_overlap = clusterProfilerGO.params$min.overlap,
+                                                                                 pAdjustMethod = clusterProfilerGO.params$p.adjust.method,
+                                                                                 similarity_filtering = clusterProfilerGO.params$do.similarity.filtering)
 
        #For GSEA,an ordered gene list must be prepared:
 
@@ -91,19 +107,20 @@ runEnrichmentAnalyses <- function(diffr_wrapper.output, analysis.name="",
        ## feature 3: decreasing order
        geneList <- sort(geneList, decreasing = TRUE)
 
-       
+
        fname.no.suffix = file.path(out.dir, paste(analysis.name, contrast, method, "GSEA", sep = "_"))
-       print(clusterProfiler.params$max.gene.set.size)
        enrichment_out.l$clusterProfiler_GSEA[[contrast]] <- run_clusterProfiler_GO(input_genes = geneList,
                                                                                   background_genes = background.genes,
                                                                                   file_name = fname.no.suffix,
-                                                                                  ordered_query = TRUE,
-                                                                                  ontology = ontologies.used,
+                                                                                  ordered_query = TRUE, #to run function in GSEA-mode
                                                                                   OrgDb = org.db,
                                                                                   pvalueCutoff = p.thr,
-                                                                                  max_set_size = clusterProfiler.params$max.gene.set.size,
-                                                                                  pAdjustMethod = "BH",
-                                                                                  similarity_filtering = clusterProfiler.params$do.similarity.filtering)
+                                                                                  ontology = clusterProfilerGO.params$ontology,
+                                                                                  min_set_size = clusterProfilerGO.params$min.gene.set.size,
+                                                                                  max_set_size = clusterProfilerGO.params$max.gene.set.size,
+                                                                                  min_overlap = clusterProfilerGO.params$min.overlap,
+                                                                                  pAdjustMethod = clusterProfilerGO.params$p.adjust.method,
+                                                                                  similarity_filtering = clusterProfilerGO.params$do.similarity.filtering)
       } 
     
       if(method == "DAVID") {
@@ -114,10 +131,11 @@ runEnrichmentAnalyses <- function(diffr_wrapper.output, analysis.name="",
                                    foreground.ensembl.ids = input.genes, 
                                    email.address = david.params$email.address,
                                    url.address = david.params$url,
-                                   time.out.value = 60000, 
-                                   annotation.category = "GOTERM_BP_FAT", 
+                                   time.out.value = david.params$time.out.value, 
+                                   annotation.category = david.params$annotation.category, 
                                    pval.thr = p.thr, 
-                                   max.gene.set.size = david.params$max.gene.set.size, save.result.table = TRUE, out.dir=out.dir,analysis.name,
+                                   max.gene.set.size = david.params$max.gene.set.size, 
+                                   save.result.table = TRUE, out.dir=out.dir,analysis.name,
                                    contrast.name=contrast)
         
         }

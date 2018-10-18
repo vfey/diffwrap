6b5719452e028f3a89475e4f61a229cba6d9504a
MeeriP <meeri.pekkarinen.genevia@gmail.com>
2018-10-18 10:31:33 +0300

    Fixed the type of output file from "fake" .xls to real .xls

--- R/doDavidEnrichmentAnalysis.R
+++ R/doDavidEnrichmentAnalysis.R
@@ -1,4 +1,5 @@
 library("RDAVIDWebService")
+library(WriteXLS)
 
 doDavidEnrichmentAnalysis = function(background.ensembl.ids,
                                      foreground.ensembl.ids,
@@ -15,63 +16,61 @@ doDavidEnrichmentAnalysis = function(background.ensembl.ids,
   
   
   #Setting up the david interface
-  david <- DAVIDWebService$new(email= email.address, url = url.address)
+  david <- DAVIDWebService$new(email = email.address, url = url.address)
   
   setTimeOut(david, time.out.value)
   
   #Setting up gene lists /ensembl_ids):
-  FG <- addList(david, foreground.ensembl.ids, idType="ENSEMBL_GENE_ID", listName="isClass", listType="Gene")
+  FG <- addList(david, foreground.ensembl.ids, idType = "ENSEMBL_GENE_ID", listName = "isClass", listType = "Gene")
   cat("      Proportion ", FG$inDavid, " of the given foreground genes in DAVID \n")
   
-  if (length(background.ensembl.ids) == 1 && background.ensembl.ids == ""){
+  if (length(background.ensembl.ids) == 1 && background.ensembl.ids == "") {
     cat("      Using DAVID default background (the complete genome) \n")
   }
   else {
-    BG <- addList(david, background.ensembl.ids, idType="ENSEMBL_GENE_ID", listName="all", listType="Background")
+    BG <- addList(david, background.ensembl.ids, idType = "ENSEMBL_GENE_ID", listName = "all", listType = "Background")
     cat("      Proportion ", BG$inDavid, " of the given background genes in DAVID \n")
   }
   
   cat("   The species is ", getSpecieNames(david), "\n" )
   
   setAnnotationCategories(david, annotation.category)
-  FuncAnnotChart <- getFunctionalAnnotationChart(david, threshold=pval.thr, count=3L) #PValue < 0.05, at least 3 genes
+  FuncAnnotChart <- getFunctionalAnnotationChart(david, threshold = pval.thr, count = 3L) #PValue < 0.05, at least 3 genes
   
   #Filtering and ordering the result based on adjusted p-value 
   filtered_FuncAnnotChart = FuncAnnotChart[FuncAnnotChart$Benjamini < pval.thr,]
-  if(dim(filtered_FuncAnnotChart)[1] > 1){
+  if (dim(filtered_FuncAnnotChart)[1] > 1) {
     filtered_FuncAnnotChart = filtered_FuncAnnotChart[order(filtered_FuncAnnotChart$Benjamini),]
   
     #filtering out lines with Large Gene sets:
-    if(max.gene.set.size != ''){
+    if (max.gene.set.size != '') {
       filtered_FuncAnnotChart = filtered_FuncAnnotChart[filtered_FuncAnnotChart$Count <= max.gene.set.size,]
     }
   
     ##Requires medseqr!! Replacing ENSMBL IDS with symbolic names if found (otherwise ENSEMBL ID is kept)
-    for( i in 1:dim(filtered_FuncAnnotChart)[1]){
-      genes = unlist(strsplit(filtered_FuncAnnotChart$Genes[i], split = ", "))
-      syms = convertId2(genes, "Human")
-      syms[is.na(syms)] = genes[is.na(syms)]
-      new_names = paste(syms,collapse="/") #writing with /-separator (easier to read from file later)
+    for (i in 1:dim(filtered_FuncAnnotChart)[1]) {
+      genes <- unlist(strsplit(filtered_FuncAnnotChart$Genes[i], split = ", "))
+      syms <- convertId2(genes, "Human")
+      syms[is.na(syms)] <- genes[is.na(syms)]
+      new_names <- paste(syms,collapse = ",") #writing with comma-separator
       filtered_FuncAnnotChart$Genes[i] = new_names
     }
   
   
     #splitting the Term-column of original output into two columns
-    GO_terms = sapply(strsplit(as.character(filtered_FuncAnnotChart$Term),'~'), "[", 1)
-    descriptions = sapply(strsplit(as.character(filtered_FuncAnnotChart$Term),'~'), "[", 2)
+    GO_terms <- sapply(strsplit(as.character(filtered_FuncAnnotChart$Term),'~'), "[", 1)
+    descriptions <- sapply(strsplit(as.character(filtered_FuncAnnotChart$Term),'~'), "[", 2)
   
-    result.table = data.frame(Term = GO_terms,
+    result.table <- data.frame(Term = GO_terms,
                             Description = descriptions)
-    result.table = cbind(result.table, filtered_FuncAnnotChart[, colnames(filtered_FuncAnnotChart) != "Term"])
+    result.table <- cbind(result.table, filtered_FuncAnnotChart[, colnames(filtered_FuncAnnotChart) != "Term"])
   
-    result.table = result.table[, c(1:2, 4, 8:10, 6, 13, 7)] #Taking only most interesting columns
+    result.table <- result.table[, c(1:2, 4, 8:10, 6, 13, 7)] #Taking only most interesting columns
   
-    if(save.result.table){
-      filename=paste0(contrast.name,".DAVID.", annotation.category,".enrichment_table.xls" )
+    if (save.result.table) {
+      filename <- paste0(contrast.name,".DAVID.", annotation.category,".enrichment_table.xls" )
       cat("      Saving DAVID result table into ", file.path(out.dir, paste0(analysis.name, ".", filename)), "...\n")
-      write.table(result.table,
-                file = file.path(out.dir, paste0(analysis.name, ".", filename)), row.names = FALSE,
-                col.names = TRUE)
+      WriteXLS(result.table, ExcelFileName = file.path(out.dir, paste0(analysis.name, ".", filename)), SheetNames = NULL, BoldHeaderRow = T)
     }
       return(result.table)
   } 

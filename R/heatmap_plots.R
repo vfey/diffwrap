75a66f5fc585ec9c08683c446d93f929313267b4
Bogdan Iancu <iancu.v.bogdan@gmail.com>
2018-11-09 19:00:16 +0200

    changes for small correlogram

--- R/heatmap_plots.R
+++ R/heatmap_plots.R
@@ -15,7 +15,6 @@
 pheatmap_plots <-
   function(d3, id, sym.col="gene_symbol", samp.info = samp.info, samples, groups, sample.plot.names,main=NULL, p.thr=0.05, fdr.thr=0.05, logfc.thr=1)
   {
-  
     # elim row with duplicate sym.col entries, set rownames to gene_symbol names
 
     d3 = as.data.table(d3)
@@ -45,7 +44,7 @@ pheatmap_plots <-
     #get the heatmap column annotation
     samp.anno = as.data.frame(groups)
     rownames(samp.anno) = samp.names
-    colnames(samp.anno)[1] = "Sample.Class"
+    colnames(samp.anno)[1] = "Sample Class"
     
     #if pretty names for the plots are available change the names to the pretty names
     if (!is.null(sample.plot.names)) {
@@ -60,9 +59,14 @@ pheatmap_plots <-
     if (nrow(dat.sign.fdr) > 0) {
       if (nrow(dat.sign.fdr) > 100) {
         dat.sign.fdr = as.data.frame(dat.sign.fdr[1:100,])
+        #make 2nd option smaller pheatmap for genes
+        dat.sign.fdr.small = as.data.frame(dat.sign.fdr[1:50,])
       }
       fdr_hm_list[["row"]] = diffr_pheatmap(dat.sign.fdr, clinical.mat = samp.anno, scale.fl = "row")
       fdr_hm_list[["none"]] = diffr_pheatmap(dat.sign.fdr, clinical.mat = samp.anno, scale.fl = "none")
+      fdr_hm_list[["rowsmall"]] = diffr_pheatmap(dat.sign.fdr.small, clinical.mat = samp.anno)
+      fdr_hm_list[["nonesmall"]] = diffr_pheatmap(dat.sign.fdr.small, clinical.mat = samp.anno)
+      
     } else {
       print("There are 0 entries with significant adjusted P-values in the differential expression data frame")
       print("Checking P-value entries...")
@@ -70,10 +74,13 @@ pheatmap_plots <-
       if (nrow(dat.sign.pv) > 0) {
         if (nrow(dat.sign.pv) > 100) {
           dat.sign.pv = as.data.frame(dat.sign.pv[1:100,])
+          dat.sign.pv.small = as.data.frame(dat.sign.pv[1:50,])
         }
         pv_hm_list[["row"]] = diffr_pheatmap(dat.sign.pv, clinical.mat = samp.anno, scale.fl = "row")
         pv_hm_list[["none"]] = diffr_pheatmap(dat.sign.pv, clinical.mat = samp.anno, scale.fl = "none")
-      } else {
+        pv_hm_list[["rowsmall"]] = diffr_pheatmap(dat.sign.pv.small, clinical.mat = samp.anno, scale.fl = "row")
+        pv_hm_list[["nonesmall"]] = diffr_pheatmap(dat.sign.pv.small, clinical.mat = samp.anno, scale.fl = "none")
+        } else {
         print("There are 0 entries with significant P-values in the differential expression data frame")
       }
       
@@ -82,11 +89,14 @@ pheatmap_plots <-
         gl.pv = list()
         gl.pv$regular = pv_hm_list$row$regular
         gl.pv$gene.correlogram = pv_hm_list$none$correlogram
+        gl.pv$gene.correlogram.small = pv_hm_list$nonesmall$correlogram
         gl.pv$samp.correlogram = pv_hm_list$none$correlogram.sample
         grid.newpage()
         print(gl.pv$regular )
         grid.newpage()
         print(gl.pv$gene.correlogram)
+        grid.newpage()
+        print(gl.pv$gene.correlogram.small)
         grid.newpage()
         print(gl.pv$samp.correlogram)
         g.l[["pval"]] = gl.pv
@@ -98,11 +108,14 @@ pheatmap_plots <-
       gl.fdr = list()
       gl.fdr$regular = fdr_hm_list$row$regular
       gl.fdr$gene.correlogram = fdr_hm_list$none$correlogram
+      gl.fdr$gene.correlogram.small = fdr_hm_list$nonesmall$correlogram
       gl.fdr$samp.correlogram = fdr_hm_list$none$correlogram.sample
       grid.newpage()
       print(gl.fdr$regular )
       grid.newpage()
       print(gl.fdr$gene.correlogram)
+      grid.newpage()
+      print(gl.fdr$gene.correlogram.small)
       grid.newpage()
       print(gl.fdr$samp.correlogram)
       g.l[["fdr"]] = gl.fdr

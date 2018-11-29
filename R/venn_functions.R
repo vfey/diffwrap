4bef5d4358e7b141135a0589089f2b6b9715c301
Bogdan Iancu <iancu.v.bogdan@gmail.com>
2018-11-29 13:06:25 +0200

    change that take the hgnc col dynamically

--- R/venn_functions.R
+++ R/venn_functions.R
@@ -3,9 +3,9 @@ library(purrr)
 library(dplyr)
 library(venn)
 library(VennDiagram)
-# compar.table1 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_CRPC-Advanced.PC_differential_expression.tsv", stringsAsFactors = TRUE))
-# compar.table2 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_Advanced.PC-Localized.PC_differential_expression.tsv", stringsAsFactors = TRUE))
-# compar.table3 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_Localized.PC-BPH_differential_expression.tsv", stringsAsFactors = TRUE))  
+ # compar.table1 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_CRPC-Advanced.PC_differential_expression.tsv", stringsAsFactors = TRUE))
+ # compar.table2 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_Advanced.PC-Localized.PC_differential_expression.tsv", stringsAsFactors = TRUE))
+ # compar.table3 <- as.data.frame(fread("./diffr_test_results_new_20181109_184403/test_run_Localized.PC-BPH_differential_expression.tsv", stringsAsFactors = TRUE))  
 
 compar.table1 <- as.data.frame(fread("./data/finvector_test_data/DEGresults_M1_vs_M0_SIGN.csv", stringsAsFactors = TRUE))
 compar.table2 <- as.data.frame(fread("./data/finvector_test_data/DEGresults_Mreg_Kiel_vs_M0_SIGN.csv", stringsAsFactors = TRUE))
@@ -27,18 +27,21 @@ list.comp.tables4 <- list("A" = compar.table1, "B" = compar.table2, "C" = compar
 diffr_venn <- function(list.comp.tables, join_vec) {
   
   #extract DEGs list from the contrast tables list 
-  DEGs.list <- lapply(list.comp.tables4, function(x) {
+  DEGs.list <- lapply(list.comp.tables, function(x) {
     #extract hgnc colname
     hgnc.col <- names(x)[grep("symbol|hgnc", tolower(names(x)))]
     #extract DEGs from table x based on hgnc.col
     DEGs <- as.character(x[[hgnc.col]])
     #remove all entry with empty string
     DEGs <- DEGs[DEGs!=""]
+    #choose only unique DEGs
+    DEGs <- unique(DEGs)
     return(DEGs)
   })
   
+  fill.color = c("darkblue", "deepskyblue", "darkturquoise", "darkorchid4")
   venn.diag <- venn.diagram(DEGs.list, 
-                            col = "transparent", fill = c("darkblue", "deepskyblue", "darkturquoise", "darkorchid4"),  height = 8000, width = 8000, print.mode = c("raw", "percent"),
+                            col = "transparent", fill = fill.color[1:length(DEGs.list)],  height = 8000, width = 8000, print.mode = c("raw", "percent"),
                             cat.cex = 1.5, cex = 2.5, imagetype = "png", filename = NULL)
   venn.sets.lists = venn(DEGs.list)
   venn.sets.intersections = attr(venn.sets.lists, "intersections")
@@ -62,23 +65,25 @@ diffr_venn <- function(list.comp.tables, join_vec) {
     #if all entries are 1 then join them all based on the join_vec
     else if (all(venn.tt[j,] == 1)) {
       print(rownames(venn.tt)[j])
-      inters.all <- list.comp.tables %>% reduce(inner_join, by = join_vec)
-      inters.all <- inters.all[inters.all$`HGNC symbol` != "",]
+      inters.all <- list.comp.tables %>% purrr::reduce(inner_join, by = join_vec)
+      hgnc.col <- names(inters.all)[grep("symbol|hgnc", tolower(names(inters.all)))]
+      inters.all <- inters.all[inters.all[hgnc.col] != "",]
       list.venn.tables[[rownames(venn.tt)[j]]] <- inters.all
       
-      print(paste0(rownames(venn.tt)[j],"-",nrow(inters.all)))
+      print(paste0(rownames(venn.tt)[j],"-",nrow(inters.all[unique(inters.all[[hgnc.col]]),])))
     }
-    #otherwise calculate the intersection for the ones that have index 1 and the union for those who have 0 and extract those entries which are in the intersection and not union
-    #this gives the 
+    #otherwise calculate the intersection for the ones that have index 1 and the union for those who have 0 and extract those entries which are in the intersection and not in the union
+    #this gives the intersection table for the Venn sections seen in the Venn diagram 
     else {
       join.part <- list.comp.tables[which(venn.tt[j,] == 1)]
-      inters <- join.part %>% reduce(dplyr::inner_join, by = join_vec)
+      inters <- join.part %>% purrr::reduce(dplyr::inner_join, by = join_vec)
       anti.part <- list.comp.tables[which(venn.tt[j,] == 0)]
-      outsect <- anti.part %>% reduce(dplyr::full_join, by = join_vec)
+      outsect <- anti.part %>% purrr::reduce(dplyr::full_join, by = join_vec)
       res.join <- inters %>% dplyr::anti_join(outsect, by = join_vec)
-      res.join <- res.join[res.join$`HGNC symbol` != "",]
+      hgnc.col <- names(res.join)[grep("symbol|hgnc", tolower(names(res.join)))]
+      res.join <- res.join[res.join[[hgnc.col]] != "",]
       list.venn.tables[[rownames(venn.tt)[j]]] <- res.join
-      print(paste0("clams, clams everywhere - ",rownames(venn.tt)[j],"--",nrow(res.join)))
+      print(paste0("clams, clams everywhere - ",rownames(venn.tt)[j],"--",nrow(res.join[unique(res.join[[hgnc.col]]),])))
     }
     
   }

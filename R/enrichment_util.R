ccb2e3e8cef9de06acc12fc95818e4964a78203a
Vidal Fey <vidal.fey@tuni.fi>
2025-06-06 22:45:55 +0300

    debugging

--- R/enrichment_util.R
+++ R/enrichment_util.R
@@ -74,7 +74,7 @@ plot_enrichment_network <- function(enrichment.result, DE.result, plot.filename,
   geneSymbolColumn = names(DE.table)[grep("ymbol", tolower(names(DE.table)))]
   logFoldChangeColumn = names(DE.table)[grep("foldch|logfc", tolower(names(DE.table)))]
   adjPvalColumn = names(DE.table)[grep("^fdr$|^adj*\\.{0,1}p\\.{0,1}val[e-u]{0,2}$", tolower(names(DE.table)))]
-  termColumn = names(enrichment.table)[grep("description|^term", tolower(names(enrichment.table)))]
+  termColumn = names(enrichment.table)[grep("description|^term_", tolower(names(enrichment.table)))]
   DEGcolumn =   names(enrichment.table)[grep("^degs|^genes|^intersection$", tolower(names(enrichment.table)))]
   cat("         Detected following columns in data inputted for network visualisation: \n")
   cat("         DE (log) fold changes: ", logFoldChangeColumn,  "\n")
@@ -143,7 +143,6 @@ plot_enrichment_network <- function(enrichment.result, DE.result, plot.filename,
   # Parse the pathway table to create edges
   edges <- c()
   genes = c()
-  browser()
   for (i in 1:length(enrichment.table[[termColumn]])) {
     cat("\n")
     x <- strsplit(as.character(enrichment.table[i, DEGcolumn]), split = ",")[[1]]
@@ -478,7 +477,7 @@ runEnrichmentAnalyses <- function(diffr.wrapper.output, analysis.name="enrichmen
          genes <- geneList
 
        }
-       cat("  Running tests...")
+       cat("  Running tests...\n")
        result <- run_clusterProfiler_GO(input_genes = genes,
                                         background_genes = background.genes,
                                         ordered_query = ordered.query,

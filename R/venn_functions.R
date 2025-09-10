0bb08d80cce0f4193515f09b10316aa98b9e64ee
Vidal Fey <vidal.fey@tuni.fi>
2025-09-10 13:37:48 +0300

    disabled logging in Venn function; cranked up version to 0.4-4

--- R/venn_functions.R
+++ R/venn_functions.R
@@ -51,7 +51,7 @@ diffr_venn <- function(list.comp.tables, join.vec, .log = FALSE) {
   if (.log) {
     futile.logger::flog.threshold(futile.logger::ERROR, name = "VennDiagramLogger")
   }
-  venn.diag <- try(VennDiagram::venn.diagram(DEGs.list,
+  venn.diag <- try(VennDiagram::venn.diagram(DEGs.list, disable.logging = !(.log),
                             col = "transparent", fill = fill.color,  height = 8000, width = 8000, print.mode = c("raw", "percent"),
                             cat.cex = 1.2, cex = 2.2, imagetype = "png", filename = NULL), silent = T)
   if (is(venn.diag, "try-error")) warning("Plotting Venn diagram failed!")

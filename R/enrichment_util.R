4d4c310d004e4f37d2fafa449c02b118684b5ea5
Vidal Fey <vidal.fey@tuni.fi>
2025-09-13 20:06:48 +0300

    bug fix in enrichment utils function where contrast folders with similar names were matched when creating per-enrichtment sub-folders; cranked up version to 0.4-10

--- R/enrichment_util.R
+++ R/enrichment_util.R
@@ -403,11 +403,11 @@ runEnrichmentAnalyses <- function(diffr.wrapper.output, analysis.name="enrichmen
 
   for (contrast in contrast.names) {
 
-    contr.out.dir  <- dir(out.dir, pattern = paste0("^",contrast), full.names = TRUE)
+    contr.out.dir  <- dir(out.dir, pattern = paste0("^", contrast, "$"), full.names = TRUE)
     #Creating sub-folders
-    browser()
+    cat("  Creating per-enrichment sub-folders...")
     lapply(enrichment.methods, function(method) dir.create(file.path(contr.out.dir, method), showWarnings = F))
-
+    cat("done\n")
 
     cat("***", contrast, "*** \n")
     de_table <- diffr.wrapper.output$contrasts[[contrast]]
